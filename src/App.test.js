515271cc9d841d2b2a4a91e6f0ef637c21b68bf4
Santosh Bharadwaj <santosh.bharadwaj.ext@bayer.com>
2018-02-15 23:08:40 +0100

    Fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -6,5 +6,5 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
-  expect(1===1);
+    expect(1).toEqual(1);
 });
